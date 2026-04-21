439aebfc7d6e5ef340a2fa49a2bb14086c7d1000
Amboara <amboaraniainarkt@gmail.com>
2026-04-21 13:38:03 +0300

    1st

---
+++ src/main/java/org/example/examprog3/ExamProg3Application.java
@@ -0,0 +1,13 @@
+package org.example.examprog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamProg3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamProg3Application.class, args);
+    }
+
+}
